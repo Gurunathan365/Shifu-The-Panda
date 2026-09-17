fec5e5bc1eac0b0eeb2e3829f1858eddcf276886
Gurunathan Ganesh <gurunathang365@gmail.com>
2026-09-17 21:38:32 +0530

    Update template file in home route

--- flask.py
+++ flask.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return (render_template('projectiletwist.html'))
+    return (render_template('projectile-challenge-v2.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
